038f6437be555bb4e30e56f51c37011ccdab8dd8
quizoverflow <jinuinssu@soongsil.ac.kr>
2024-02-29 19:04:18 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Loading());
+}
+
+class Loading extends StatelessWidget {
+  const Loading ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
